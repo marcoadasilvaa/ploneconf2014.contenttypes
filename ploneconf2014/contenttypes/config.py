2622ef9e7f58156c0fe82a1c9e73368b661c3faf
Marco Da Silva <markmarco16@hotmail.com>
2014-07-18 15:46:07 -0430

    Integracion con Unittest

---
+++ ploneconf2014/contenttypes/config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Contains constants used by setuphandler.py
+"""	
+
+PROJECTNAME = 'ploneconf2014.contenttypes'
+
+DEPENDENCIES = [
+    'plone.api',
+	'setuptools',
+    'plone.app.dexterity [grok]',
+    'plone.namedfile [blobs]',
+    ]
